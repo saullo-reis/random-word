5f8484f458437b02d0dcb590ad68c34613f1e147
Saullo <saulloreis01@hotmail.com>
2025-01-29 22:48:46 -0300

    adding new feat

--- main.go
+++ main.go
@@ -14,20 +14,23 @@ import (
 )
 
 type DailyWord struct {
-	mu   sync.Mutex
-	word string
+	mu         sync.Mutex
+	word       string
+	lastUpdate time.Time
 }
 
 var dailyWord DailyWord
 
 func main() {
-	if err := refreshWord(); err != nil {
+	if err := refreshWordIfNeeded(); err != nil {
 		log.Fatalf("Erro ao carregar palavra inicial: %v", err)
 	}
 	go scheduleDailyReset()
 
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "./src")
+
+	router.LoadHTMLGlob("templates/index.html")
 
 	router.GET("/", func(c *gin.Context) {
 		dailyWord.mu.Lock()
@@ -42,28 +45,36 @@ func main() {
 	router.Run(":8080")
 }
 
-func refreshWord() error {
-	newWord, err := chooseTheWord()
+func refreshWordIfNeeded() error {
+	now := time.Now().UTC()
+	lastMidnight := getLastMidnight(now)
+
+	dailyWord.mu.Lock()
+	defer dailyWord.mu.Unlock()
+
+	if dailyWord.lastUpdate.After(lastMidnight) {
+		return nil
+	}
+
+	newWord, err := chooseTheWord(lastMidnight)
 	if err != nil {
 		return err
 	}
 
-	dailyWord.mu.Lock()
 	dailyWord.word = newWord
-	dailyWord.mu.Unlock()
-
+	dailyWord.lastUpdate = lastMidnight
 	return nil
 }
 
 func scheduleDailyReset() {
 	for {
-		now := time.Now()
-		nextMidnight := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location())
+		now := time.Now().UTC()
+		nextMidnight := getLastMidnight(now).Add(24 * time.Hour)
 		durationUntilMidnight := nextMidnight.Sub(now)
 
 		<-time.After(durationUntilMidnight)
 
-		if err := refreshWord(); err != nil {
+		if err := refreshWordIfNeeded(); err != nil {
 			log.Printf("Erro ao atualizar palavra: %v", err)
 		} else {
 			log.Println("Palavra do dia atualizada com sucesso")
@@ -71,7 +82,7 @@ func scheduleDailyReset() {
 	}
 }
 
-func chooseTheWord() (string, error) {
+func chooseTheWord(referenceTime time.Time) (string, error) {
 	file, err := os.Open("words.json")
 	if err != nil {
 		return "", fmt.Errorf("erro ao abrir arquivo: %w", err)
@@ -88,9 +99,14 @@ func chooseTheWord() (string, error) {
 		return "", fmt.Errorf("nenhuma palavra disponível no arquivo")
 	}
 
-	source := rand.NewSource(time.Now().UnixNano())
+	seed := referenceTime.Unix()
+	source := rand.NewSource(seed)
 	r := rand.New(source)
 	randomIndex := r.Intn(len(words))
 
 	return words[randomIndex], nil
+}
+
+func getLastMidnight(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC)
 }
