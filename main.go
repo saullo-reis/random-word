3379e41bcb685a2671974cae8b7d08a15f561bee
vitin <xdvitor13@gmail.com>
2025-01-30 16:14:29 -0300

    Mudança no projeto todo porque tava tudo errado

--- main.go
+++ main.go
@@ -3,29 +3,132 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-type DailyWord struct {
-	mu         sync.Mutex
-	word       string
-	lastUpdate time.Time
+type Estado struct {
+	LastDate   string `json:"last_date"`
+	RandomWord string `json:"random_word"`
 }
 
-var dailyWord DailyWord
+type Palavras struct {
+	Palavras []string `json:"palavras"`
+}
+
+func carregarPalavras() ([]string, error) {
+
+	file, err := ioutil.ReadFile("palavras.json")
+	if err != nil {
+		return nil, err
+	}
+
+	
+	var p Palavras
+	err = json.Unmarshal(file, &p)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.Palavras, nil
+}
+
+func carregarEstado() (*Estado, error) {
+	
+	file, err := ioutil.ReadFile("estado.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return &Estado{}, nil 
+		}
+		return nil, err
+	}
+
+	
+	var e Estado
+	err = json.Unmarshal(file, &e)
+	if err != nil {
+		return nil, err
+	}
+
+	return &e, nil
+}
+
+func salvarEstado(estado *Estado) error {
+	
+	data, err := json.MarshalIndent(estado, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile("estado.json", data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func obterPalavraAleatoria(palavras []string) string {
+	rand.Seed(time.Now().UnixNano())
+	return palavras[rand.Intn(len(palavras))]
+}
 
 func main() {
-	if err := refreshWordIfNeeded(); err != nil {
-		log.Fatalf("Erro ao carregar palavra inicial: %v", err)
+	
+	palavras, err := carregarPalavras()
+	if err != nil {
+		fmt.Println("Erro ao carregar palavras:", err)
+		return
 	}
-	go scheduleDailyReset()
+
+	
+	estado, err := carregarEstado()
+	if err != nil {
+		fmt.Println("Erro ao carregar estado:", err)
+		return
+	}
+
+	
+	dataAtual := time.Now().Format("2006-01-02")
+
+	
+	if estado.LastDate != dataAtual {
+		
+		estado.RandomWord = obterPalavraAleatoria(palavras)
+		estado.LastDate = dataAtual
+
+		
+		err = salvarEstado(estado)
+		if err != nil {
+			fmt.Println("Erro ao salvar estado:", err)
+			return
+		}
+	}
+
+	ehPalavraValida := false
+	for _, palavra := range palavras {
+		if palavra == estado.RandomWord {
+			ehPalavraValida = true
+			break
+		}
+	}
+	if !ehPalavraValida {
+		estado.RandomWord = obterPalavraAleatoria(palavras)
+	}
+
+	estado.LastDate = dataAtual
+
+	
+	err = salvarEstado(estado)
+	if err != nil {
+		fmt.Println("Erro ao salvar estado:", err)
+		return
+	}
 
 	router := gin.Default()
 	router.Static("/static", "./src")
@@ -33,80 +136,11 @@ func main() {
 	router.LoadHTMLGlob("templates/index.html")
 
 	router.GET("/", func(c *gin.Context) {
-		dailyWord.mu.Lock()
-		currentWord := dailyWord.word
-		dailyWord.mu.Unlock()
 
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Palavra": currentWord,
+			"Palavra": estado.RandomWord,
 		})
 	})
 
 	router.Run(":8080")
-}
-
-func refreshWordIfNeeded() error {
-	now := time.Now().UTC()
-	lastMidnight := getLastMidnight(now)
-
-	dailyWord.mu.Lock()
-	defer dailyWord.mu.Unlock()
-
-	if dailyWord.lastUpdate.After(lastMidnight) {
-		return nil
-	}
-
-	newWord, err := chooseTheWord(lastMidnight)
-	if err != nil {
-		return err
-	}
-
-	dailyWord.word = newWord
-	dailyWord.lastUpdate = lastMidnight
-	return nil
-}
-
-func scheduleDailyReset() {
-	for {
-		now := time.Now().UTC()
-		nextMidnight := getLastMidnight(now).Add(24 * time.Hour)
-		durationUntilMidnight := nextMidnight.Sub(now)
-
-		<-time.After(durationUntilMidnight)
-
-		if err := refreshWordIfNeeded(); err != nil {
-			log.Printf("Erro ao atualizar palavra: %v", err)
-		} else {
-			log.Println("Palavra do dia atualizada com sucesso")
-		}
-	}
-}
-
-func chooseTheWord(referenceTime time.Time) (string, error) {
-	file, err := os.Open("words.json")
-	if err != nil {
-		return "", fmt.Errorf("erro ao abrir arquivo: %w", err)
-	}
-	defer file.Close()
-
-	var words []string
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(&words); err != nil {
-		return "", fmt.Errorf("erro ao decodificar JSON: %w", err)
-	}
-
-	if len(words) == 0 {
-		return "", fmt.Errorf("nenhuma palavra disponível no arquivo")
-	}
-
-	seed := referenceTime.Unix()
-	source := rand.NewSource(seed)
-	r := rand.New(source)
-	randomIndex := r.Intn(len(words))
-
-	return words[randomIndex], nil
-}
-
-func getLastMidnight(t time.Time) time.Time {
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC)
 }
